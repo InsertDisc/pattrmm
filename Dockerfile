75d7abf68a45abdaf4e131a989f3d9a410c82f87
InsertDisc <31751462+InsertDisc@users.noreply.github.com>
2023-09-20 17:21:44 -0400

    Add files via upload
    Merge docker branch to repository

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# For more information, please refer to https://aka.ms/vscode-docker-python
+FROM python:3.10-slim
+
+# Keeps Python from generating .pyc files in the container
+ENV PYTHONDONTWRITEBYTECODE=1
+
+# Turns off buffering for easier container logging
+ENV PYTHONUNBUFFERED=1
+
+# Set pattrmm environment to docker
+ENV PATTRMM_DOCKER "True"
+
+
+# Install pip requirements
+COPY requirements.txt .
+RUN python -m pip install -r requirements.txt
+
+ADD main.py .
+ADD vars.py .
+ADD pattrmm.py .
+
+# During debugging, this entry point will be overridden. For more information, please refer to https://aka.ms/vscode-docker-python-debug
+ENTRYPOINT ["python", "main.py"]
+
---
+++ vars.py
@@ -0,0 +1,413 @@
+from ruamel.yaml import YAML
+yaml = YAML()
+import xml.etree.ElementTree as ET
+import requests
+import json
+import re
+import datetime
+today = datetime.datetime.today()
+import os
+library = ""
+
+is_docker = os.environ.get('PATTRMM_DOCKER', "False")
+
+if is_docker == "True":
+    configPathPrefix = "./config/"
+    
+
+if is_docker == "False":
+    configPathPrefix = "../"
+
+import logging
+log_file = "data/logs/pattrmm.log"
+logging.basicConfig(filename=log_file, level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
+
+
+config_path = configPathPrefix + 'config.yml'
+settings_path = 'preferences/settings.yml'
+
+class Plex:
+    def __init__(self, plex_url, plex_token, tmdb_api_key):
+        self.plex_url = plex_url
+        self.plex_token = plex_token
+        self.tmdb_api_key = tmdb_api_key
+        self.context = None
+
+    @property
+    def show(self):
+        self.context = 'show'
+        return self  # Return self to allow method chaining
+
+    def id(self, show_name):
+        if self.context == 'show':
+            try:
+                # Replace with the correct section ID and library URL
+                section_id = plexGet(library)  # Replace with the correct section ID
+                library_url = f"{self.plex_url}/library/sections/{section_id}/all"
+                library_url = re.sub("0//", "0/", library_url)
+                headers = {"X-Plex-Token": self.plex_token}
+                response = requests.get(library_url, headers=headers)
+
+                if response.status_code == 200:
+                    data = response.json()
+                    for item in data['MediaContainer']['Metadata']:
+                        if item['type'] == 'show' and item['title'] == show_name:
+                            return f"ID for show '{show_name}': {item['ratingKey']}"
+                    return f"Show '{show_name}' not found"
+                else:
+                    return f"Error: {response.status_code} - {response.text}"
+            except Exception as e:
+                return f"Error: {str(e)}"
+
+    def tmdb_id(self, rating_key):
+        # Attempt to retrieve TMDB ID from Plex
+        plex_tmdb_id = self.get_tmdb_id_from_plex(rating_key)
+        
+        if plex_tmdb_id is not None:
+            return plex_tmdb_id
+        
+        # If not found in Plex, search TMDB
+        if plex_tmdb_id == None:
+            show_name = self.get_show_name(rating_key)
+            year = self.year(rating_key)
+            if year != None:
+                print("")
+                print("No TMDB ID found locally: Searching for " + show_name + " with year " + str(year))
+                logging.info("No TMDB ID found locally: Searching for " + show_name + " with year " + str(year))
+                search =  self.search_tmdb_id(show_name, year)
+                if search == None:
+                    year = int(year)
+                    year += 1
+                    print("No results, searching again with year " + str(year))
+                    logging.info("No results, searching again with year " + str(year))
+                    search = self.search_tmdb_id(show_name, str(year))
+                    if search == None:              
+                         year -= 2
+                         print("No results, searching again with year " + str(year))
+                         logging.info("No results, searching again with year " + str(year))
+                         search = self.search_tmdb_id(show_name, str(year))
+                         if search == None:
+                              print(show_name + " could not be matched.")
+                              logging.info(show_name + " could not be matched.")
+                              search = "null"
+                              
+                return search
+                
+            if year == None:
+                print("")
+                print("No originally availabe year for " + show_name + ", cannot search for title reliably.")
+                logging.warning("No originally availabe year for " + show_name + ", cannot search for title reliably.")
+                search = "null"
+                return search
+            
+
+
+    def get_tmdb_id_from_plex(self, rating_key):
+        try:
+            show_details_url = f"{self.plex_url}/library/metadata/{rating_key}"
+            show_details_url = re.sub("0//", "0/", show_details_url)
+            headers = {"X-Plex-Token": self.plex_token}
+            response = requests.get(show_details_url, headers=headers)
+            if response.status_code == 200:
+                root = ET.fromstring(response.text)
+                guid_elements = root.findall('.//Guid')
+                for guid_element in guid_elements:
+                    if guid_element.get('id', '').startswith('tmdb://'):
+                        tmdb_id = guid_element.get('id')[7:]
+                        #tmdb_id = guid.split('tmdb://')[1]
+                        return tmdb_id
+                return None
+            else:
+                return f"Error: {response.status_code} - {response.text}"
+        except Exception as e:
+            return f"Error: {str(e)}"
+
+
+    def get_show_name(self, rating_key):
+        try:
+            show_details_url = f"{self.plex_url}/library/metadata/{rating_key}"
+            show_details_url = re.sub("0//", "0/", show_details_url)
+            headers = {"X-Plex-Token": self.plex_token,
+                       "accept": "application/json"
+            }
+            
+            # Make a request to get show details
+            response = requests.get(show_details_url, headers=headers)
+            
+            if response.status_code == 200:
+               data = json.loads(json.dumps(response.json()))
+               values = data['MediaContainer']['Metadata']
+               for result in values:
+                   title = result['title']
+                   title = re.sub("\s\(.*?\)","", title)
+               return title
+            else:
+                return f"Error: {response.status_code} - {response.text}"
+        except Exception as e:
+            return f"Error: {str(e)}"
+
+    def retry_search_with_adjusted_years(self, title, year):
+        for i in range(2):
+            if i == 0:
+                year += 1
+            elif i == 1:
+                year -= 2
+
+        tmdb_search_result = self.search_tmdb_id(title, year)
+        if tmdb_search_result is not None:
+            return tmdb_search_result
+
+        return "null"
+        
+
+    def year(self, rating_key):
+        try:
+            # Get the originally available year from Plex
+            show_details_url = f"{self.plex_url}/library/metadata/{rating_key}"
+            show_details_url = re.sub("0//", "0/", show_details_url)
+            headers = {"X-Plex-Token": self.plex_token,
+                       "accept": "application/json"}
+            
+            response = requests.get(show_details_url, headers=headers)
+            
+            if response.status_code == 200:
+                data = json.loads(json.dumps(response.json()))
+                for result in data['MediaContainer']['Metadata']:
+                    try:
+                        year = result['originallyAvailableAt'][:4]
+                    except KeyError:
+                        year = None
+                return year
+            else:
+                return None
+        except Exception as e:
+            return None
+
+    def search_tmdb_id(self, title, year):
+        try:
+            # Query TMDB to search for a show based on title and year
+            tmdb_api_url = "https://api.themoviedb.org/3/search/tv"
+            tmdb_api_key = self.tmdb_api_key
+            tmdb_headers = {
+            'accept': 'application/json'
+            }
+            tmdb_params = {
+                "api_key": tmdb_api_key,
+                "query": title,
+                "first_air_date_year": year
+            }
+            tmdb_response = requests.get(tmdb_api_url, headers=tmdb_headers, params=tmdb_params)
+
+            if tmdb_response.status_code == 200:
+                tmdb_data = json.loads(json.dumps(tmdb_response.json()))
+                if tmdb_data['total_results'] > 0:
+                    for item in tmdb_data['results']:
+                        if item['first_air_date'][:4] == year:
+                            id = item['id']
+                            
+                            return id
+                if tmdb_data['total_results'] == 0:
+                    return None
+
+        except Exception as e:
+            return e
+
+
+    def episodes(self, rating_key):
+        try:
+            # Retrieve a list of episodes for a show based on rating key
+            episodes_url = f"{self.plex_url}/library/metadata/{rating_key}/allLeaves"
+            episodes_url = re.sub("0//", "0/", episodes_url)
+            headers = {"X-Plex-Token": self.plex_token,
+                       "accept": "application/json"}
+            response = requests.get(episodes_url, headers=headers)
+
+            if response.status_code == 200:
+                tree = ET.ElementTree(ET.fromstring(response.text))
+                root = tree.getroot()
+                episodes = []
+                for video in root.iter('Video'):
+                    if video.get('type') == 'episode':
+                        episodes.append(video.get('title'))
+                return episodes
+            else:
+                return None
+        except Exception as e:
+            return None
+
+def read_config():
+    config_file = config_path
+    with open(config_file, "r") as yaml_file:
+        config = yaml.load(yaml_file)
+        plex_url = config['plex']['url']
+        plex_token = config['plex']['token']
+        tmdb_api_key = config['tmdb']['apikey']
+    return plex_url, plex_token, tmdb_api_key
+
+if __name__ == "__main__":
+    plex_url, plex_token, tmdb_api_key = read_config()
+    if plex_url and plex_token and tmdb_api_key:
+        plex = Plex(plex_url, plex_token, tmdb_api_key)
+
+def history(library, stat):
+        stats = "./data/history/" + library + "-history.json"
+        stats = re.sub(" ", "-", stats)
+        statsFile = open(stats, "r")
+        try:
+            statsData = json.load(statsFile)
+            statsFile.close()
+            if stat == "lastFull":
+                lastRefresh = statsData['lastRefresh']
+        except:
+            lastRefresh = today
+        return lastRefresh
+
+def librarySetting(library, value):
+
+        yaml = YAML()
+        settings = settings_path
+        with open(settings) as sf:
+            pref = yaml.load(sf)
+            if value == 'refresh':
+                try:
+                    entry = pref['libraries'][library]['refresh']
+                except KeyError:
+                    entry = 15
+            if value == 'days':
+                try:
+                    entry = pref['libraries'][library]['days_ahead']
+                    if entry > 90:
+                        entry = 90
+                except:
+                    entry = 30
+        return entry
+
+def setting(value):
+        yaml = YAML()
+        settings = settings_path
+        with open(settings) as sf:
+            pref = yaml.load(sf)
+        
+            if value == 'rsback_color':
+                entry = pref['returning_soon_bgcolor']
+            if value == 'rsfont_color':
+                entry = pref['returning_soon_fontcolor']
+            if value == 'prefix':
+                entry = pref['overlay_prefix']
+            if value == 'dateStyle':
+                entry = pref['date_style']
+            if value == 'zeros':
+                try:
+                    entry = pref['leading_zeros']
+                except:
+                    entry = True
+            if value == 'ovNew':
+                try:
+                    entry = pref['extra_overlays']['new']['use']
+                except:
+                    entry = False
+            if value == 'ovNewColor':
+                 entry = pref['extra_overlays']['new']['bgcolor']
+            if value == 'ovNewFontColor':
+                 entry = pref['extra_overlays']['new']['font_color']
+            if value == 'ovNewText':
+                 entry = pref['extra_overlays']['new']['text']
+            if value == 'ovReturning':
+                try:
+                    entry = pref['extra_overlays']['returning']['use']
+                except:
+                    entry = False
+            if value == 'ovReturningColor':
+                 entry = pref['extra_overlays']['returning']['bgcolor']
+            if value == 'ovReturningFontColor':
+                 entry = pref['extra_overlays']['returning']['font_color']
+            if value == 'ovReturningText':
+                 entry = pref['extra_overlays']['returning']['text']
+            if value == 'ovAiring':
+                try:
+                    entry = pref['extra_overlays']['airing']['use']
+                except:
+                    entry = False
+            if value == 'ovAiringColor':
+                 entry = pref['extra_overlays']['airing']['bgcolor']
+            if value == 'ovAiringFontColor':
+                 entry = pref['extra_overlays']['airing']['font_color']
+            if value == 'ovAiringText':
+                 entry = pref['extra_overlays']['airing']['text']
+            if value == 'ovEnded':
+                try:
+                    entry = pref['extra_overlays']['ended']['use']
+                except:
+                    entry = False
+            if value == 'ovEndedColor':
+                 entry = pref['extra_overlays']['ended']['bgcolor']
+            if value == 'ovEndedFontColor':
+                 entry = pref['extra_overlays']['ended']['font_color']
+            if value == 'ovEndedText':
+                 entry = pref['extra_overlays']['ended']['text']
+            if value == 'ovCanceled':
+                try:
+                    entry = pref['extra_overlays']['canceled']['use']
+                except:
+                    entry = False
+            if value == 'ovCanceledColor':
+                 entry = pref['extra_overlays']['canceled']['bgcolor']
+            if value == 'ovCanceledFontColor':
+                 entry = pref['extra_overlays']['canceled']['font_color']
+            if value == 'ovCanceledText':
+                 entry = pref['extra_overlays']['canceled']['text']   
+        return entry
+
+def traktApi(type):
+        yaml = YAML()
+        config = config_path
+        with open(config) as fp:
+            trakt = yaml.load(fp)
+            if type == 'token':
+                key = trakt['trakt']['authorization']['access_token']
+            if type == 'client':
+                key = trakt['trakt']['client_id']
+            if type == 'secret':
+                key = trakt['trakt']['client_secret']
+            if type == 'me':
+                api = traktApi('client')
+                access = traktApi('token')
+                headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + access + '',
+                'trakt-api-version': '2',
+                'trakt-api-key': '' + api + ''
+                }
+                key = json.loads(json.dumps(requests.get('https://api.trakt.tv/users/me', headers=headers).json()))['username']
+        return key
+
+def tmdbApi(var):
+        yaml = YAML()
+        config = config_path
+        with open(config) as fp:
+            tmdb = yaml.load(fp)
+            if var == 'token':
+                key = tmdb['tmdb']['apikey']
+        return key
+
+def plexApi(vix):
+        yaml = YAML()
+        config = config_path
+        with open(config) as fp:
+            plex = yaml.load(fp)
+            if vix == 'url':
+                key = plex['plex']['url']
+            if vix == 'token':
+                 key = plex['plex']['token']
+        return key
+
+
+def plexGet(identifier):
+        URL =  plexApi('url') + '/library/sections/?X-Plex-Token=' + plexApi('token')
+        libraries = re.sub("0//", "0/", URL)
+        libSearch = ET.fromstring(requests.get(libraries).text)
+        for directory in libSearch.findall('Directory'):
+            if directory.get('title') == identifier:
+                key = directory.get('key')
+                title = directory.get('title')
+        return key
